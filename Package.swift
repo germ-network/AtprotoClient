670f5f830e9972715172506c45ea2c64c5b19ace
Mark Xue <mark@germ.network>
2026-03-28 00:45:22 -0700

    define the atproto-proxy header here instead of atprotoTypes

--- Package.swift
+++ Package.swift
@@ -16,8 +16,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/germ-network/AtprotoTypes.git",
-			//			from: "0.0.4"
-			branch: "mark/http-types"
+			from: "0.0.4"
 		),
 		.package(
 			url: "https://github.com/germ-network/GermConvenience.git",
