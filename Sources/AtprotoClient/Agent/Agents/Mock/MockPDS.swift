093f3daf4cf84b443e41513be6a1b2e4a44cb3c8
Mark Xue <mark@germ.network>
2026-04-08 15:20:32 -0700

    add handling for .well-known oauth paths

--- Sources/AtprotoClient/Agent/Agents/Mock/MockPDS.swift
+++ Sources/AtprotoClient/Agent/Agents/Mock/MockPDS.swift
@@ -73,20 +73,39 @@ public actor MockPDS {
 	) async throws -> HTTPDataResponse {
 		let request = try requestComponents.constructUrl(serviceUrl: serviceUrl)
 		let requestUrl = try request.request.url.tryUnwrap
-		let pathComponents = requestUrl.pathComponents
-
-		// pathComponents[0] is "/"
-		guard pathComponents[1] == "xrpc" else {
-			throw HTTPResponseError.unsuccessfulString(400, "InvalidRequest")
-		}
-
+		
+		
 		let components = try URLComponents(
 			url: requestUrl,
 			resolvingAgainstBaseURL: false
 		).tryUnwrap
 		let queryParameters = try components.queryItems.tryUnwrap.asDictionary
+		
+		let pathComponents = requestUrl.pathComponents
+		
+		switch pathComponents[1] {
+		case "xrpc":
+			return try await handleXrpc(
+				xrpcNsid: pathComponents[2],
+				queryParameters: queryParameters,
+				body: requestComponents.body,
+				auth: auth
+			)
+		case ".well-known":
+			return try await handleWellKnown(path: .init(pathComponents[2...]))
+		default:
+			throw HTTPResponseError.unsuccessfulString(400, "InvalidRequest")
+		}
 
-		let xrpcNsid = pathComponents[2]
+		//here is where a directory of types would be handy
+	}
+	
+	private func handleXrpc(
+		xrpcNsid: Atproto.NSID,
+		queryParameters: [String: String],
+		body: Data?,
+		auth: Bool
+	) async throws -> HTTPDataResponse {
 		switch xrpcNsid {
 		case Lexicon.Com.Atproto.Repo.getRecordNSID:
 			return try await getRecord(queryParameters: queryParameters)
@@ -100,14 +119,49 @@ public actor MockPDS {
 			}
 
 			return try await putRecord(
-				bodyData: requestComponents.body.tryUnwrap
+				bodyData: body.tryUnwrap
 			)
 		default:
 			throw HTTPResponseError.unsuccessfulString(400, "InvalidRequest")
 		}
-
-		//here is where a directory of types would be handy
 	}
+	
+	private func handleWellKnown(path: [String]) async throws -> HTTPDataResponse {
+		guard let component = path.first, path.count == 1 else {
+			throw HTTPResponseError.unsuccessfulString(400, "InvalidRequest")
+		}
+		switch component {
+		case "oauth-protected-resource":
+			return .init(
+				data: try mockProtectedResourceMetadata,
+				response: .init(status: .ok)
+			)
+		case "oauth-authorization-server":
+			return .init(
+				data: Self.mockAuthMetadata.utf8Data,
+				response: .init(status: .ok)
+			)
+		default:
+			throw HTTPResponseError.unsuccessfulString(400, "InvalidRequest")
+		}
+	}
+	
+	private var mockProtectedResourceMetadata: Data {
+		get throws {
+			try JSONSerialization.data(withJSONObject: [
+				"resource": serviceUrl.absoluteString,
+				"authorization_servers": [serviceUrl.absoluteString],
+				"scopes_supported":[],
+				"bearer_methods_supported":["header"],
+				"resource_documentation":"https://atproto.com"
+			])
+		}
+	}
+	
+	private static let mockAuthMetadata =
+	"""
+	{"issuer":"https://bsky.social","request_parameter_supported":true,"request_uri_parameter_supported":true,"require_request_uri_registration":true,"scopes_supported":["atproto","transition:email","transition:generic","transition:chat.bsky"],"subject_types_supported":["public"],"response_types_supported":["code"],"response_modes_supported":["query","fragment","form_post"],"grant_types_supported":["authorization_code","refresh_token"],"code_challenge_methods_supported":["S256"],"ui_locales_supported":["en-US"],"display_values_supported":["page","popup","touch"],"request_object_signing_alg_values_supported":["RS256","RS384","RS512","PS256","PS384","PS512","ES256","ES256K","ES384","ES512","none"],"authorization_response_iss_parameter_supported":true,"request_object_encryption_alg_values_supported":[],"request_object_encryption_enc_values_supported":[],"jwks_uri":"https://bsky.social/oauth/jwks","authorization_endpoint":"https://bsky.social/oauth/authorize","token_endpoint":"https://bsky.social/oauth/token","token_endpoint_auth_methods_supported":["none","private_key_jwt"],"token_endpoint_auth_signing_alg_values_supported":["RS256","RS384","RS512","PS256","PS384","PS512","ES256","ES256K","ES384","ES512"],"revocation_endpoint":"https://bsky.social/oauth/revoke","pushed_authorization_request_endpoint":"https://bsky.social/oauth/par","require_pushed_authorization_requests":true,"dpop_signing_alg_values_supported":["RS256","RS384","RS512","PS256","PS384","PS512","ES256","ES256K","ES384","ES512"],"client_id_metadata_document_supported":true,"prompt_values_supported":["none","login","consent","select_account","create"]}
+	"""
 
 	private func getRecord(
 		queryParameters: [String: String]
