13dcbb5711e1b722fd3037d37f5afa3a45353100
Mark Xue <mark@germ.network>
2026-05-30 10:19:52 -0700

    update to germ convenience 0.2.4 for android and linux build

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
 		),
 		.package(
 			url: "https://github.com/germ-network/GermConvenience.git",
-			from: "0.2.1"
+			from: "0.2.4"
 		),
 		.package(
 			url: "https://github.com/apple/swift-crypto.git",
